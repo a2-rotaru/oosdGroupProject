44bd41cd6409cf7a34e5558d20f530db425483f2
Alexu <Alexu@DESKTOP-B5NRTRT>
2020-02-11 14:04:54 +0000

    Hall modified

--- src/oosd2groupproject2/Hall.java
+++ src/oosd2groupproject2/Hall.java
@@ -4,11 +4,25 @@
  * and open the template in the editor.
  */
 package oosd2groupproject2;
-
+import java.util.ArrayList;
 /**
  *
  * @author sakla
  */
 public class Hall {
+    
+    private String name;
+    private int number;
+    private String address;
+    private ArrayList rooms;
+    
+    //Set constructor 
+    public Hall(String name, int number, String address) {
+        this.name = name;
+        
+    }
+    
+    
+        
     
 }
