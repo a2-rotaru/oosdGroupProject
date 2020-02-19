5629eb0118725ef61810dea89f5896d7400d5a9a
Alexu <Alexu@DESKTOP-B5NRTRT>
2020-02-19 15:48:05 +0000

    fxml file added for login

--- src/oosd2groupproject2/LoginViewController.java
+++ src/oosd2groupproject2/LoginViewController.java
@@ -13,6 +13,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.fxml.FXML;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -30,11 +37,50 @@ public class LoginViewController implements Initializable {
 
     @FXML
     private PasswordField passwordField;
-
+    
     @FXML
-    void Login(ActionEvent event) {
-
+    private Label labelField;
+    
+  
+    @FXML
+    void Login(ActionEvent event) throws IOException {
+        if ((usernameField.getText().equals("Jon-Snow")) && (passwordField.getText().equals("password")))
+        {
+            labelField.setText("Welcome!");
+            changeStage(event);
+        }
+        else if ((usernameField.getText().equals("Alex-Spurell")) && (passwordField.getText().equals("wardenAlex")))
+        {
+           labelField.setText("Welcome!");
+           changeStage(event);
+        }
+        else if ((usernameField.getText().equals("Connor-Gee")) && (passwordField.getText().equals("wardenConnor")))
+        {
+           labelField.setText("Welcome!");
+           changeStage(event);
+        }
+        else if ((usernameField.getText().equals("Ash-Brian")) && (passwordField.getText().equals("wardenAsh")))
+        {
+           labelField.setText("Welcome!");
+           changeStage(event);
+        }
+        else
+        {
+            labelField.setText("Wrong username or password!");
+        }
+        
+        }
+     public void changeStage(ActionEvent event) throws IOException{
+        Parent mainView = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+        Scene mainViewScene = new Scene(mainView);
+            
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(mainViewScene);
+        window.show();
+        window.centerOnScreen();
     }
+     
+   
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
