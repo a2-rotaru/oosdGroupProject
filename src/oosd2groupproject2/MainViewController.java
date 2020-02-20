8e722ffc11fae39a58c432961e25346dad7fdeb5
Alexu <Alexu@DESKTOP-B5NRTRT>
2020-02-20 12:52:41 +0000

    MainView started plus photo

--- src/oosd2groupproject2/MainViewController.java
+++ src/oosd2groupproject2/MainViewController.java
@@ -7,7 +7,14 @@ package oosd2groupproject2;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,9 +23,92 @@ import javafx.fxml.Initializable;
  */
 public class MainViewController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+   
+    @FXML
+    private ComboBox<?> hallBox;
+
+    @FXML
+    private TextField addressBox;
+
+    @FXML
+    private TextField telephoneBox;
+
+    @FXML
+    private TextField hallNoBox;
+
+    @FXML
+    private TextField no_roomsBox;
+
+    @FXML
+    private TextField wardenBox;
+
+    @FXML
+    private TextField firstnameBox;
+
+    @FXML
+    private TextField lastnameBox;
+
+    @FXML
+    private TextField studentIDBox;
+
+    @FXML
+    private TextField roomNoBox;
+
+    @FXML
+    private TextField rateBox;
+
+    @FXML
+    private ComboBox<?> cleaningBox;
+
+    @FXML
+    private ComboBox<?> statusBox;
+
+    @FXML
+    private TextField leaseIDBox;
+
+    @FXML
+    private TextField durationBox;
+
+    @FXML
+    private Tab tabManager;
+
+    @FXML
+    private TableColumn<?, ?> columnFirstName;
+
+    @FXML
+    private TableColumn<?, ?> columnLastName;
+
+    @FXML
+    private TableColumn<?, ?> columnStudentID;
+
+    @FXML
+    private TableColumn<?, ?> columnHall;
+
+    @FXML
+    private TableColumn<?, ?> columnRoomNo;
+
+    @FXML
+    private Tab tabWarden;
+
+    @FXML
+    private TableColumn<?, ?> columnStatus;
+
+    @FXML
+    void applyChanges(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createLease(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteLease(ActionEvent event) {
+
+    }
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
