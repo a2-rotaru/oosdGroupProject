77b74674505a3e090fbfef9a919f8b30a05032a5
Alexu <Alexu@DESKTOP-B5NRTRT>
2020-02-20 10:28:01 +0000

    fxml file added for login

--- src/oosd2groupproject2/MainApplication.java
+++ src/oosd2groupproject2/MainApplication.java
@@ -11,12 +11,15 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import oosd2groupproject2.Hall;
-import oosd2groupproject2.Manager;
+//import oosd2groupproject2.Manager;
 import oosd2groupproject2.RentalAgreement;
 import oosd2groupproject2.Room;
 import oosd2groupproject2.Student;
@@ -30,50 +33,38 @@ import oosd2groupproject2.Status;
  */
 public class MainApplication extends Application{
     
-public ArrayList<Hall> hallArray = new ArrayList<Hall>();
+//public ArrayList<Hall> hallArray = new ArrayList<Hall>();
     
     
-    Manager Manager = new Manager("Jon Snow" , "password");
+   /* Manager Manager = new Manager("Jon Snow" , "password");
     Hall hall1 = new Hall("Frenchay" , 1, 450, "Filton Road, Bs16 1ZG",new Warden("Alex Spurell","warden1"));
     Hall hall2 = new Hall("Glenside", 2, 400, "Blackberry Hill, Bs16 1DD", new Warden("Kane Bob", "warden2"));
     Hall hall3 = new Hall("Student Village", 3, 550, "307 Coldharbour Ln. Bs16 1ZH", new Warden("Juan Ki", "warden3"));
+    */
     
-    
-    public MainApplication() {
+   /* public MainApplication() {
         //Create hall Array, initialise halls and add some data
         hallArray.add(hall1);
         hallArray.add(hall2);
         hallArray.add(hall3);
         
-    }
+    } */
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+        stage.setTitle("UWE Accommodation System");
+        Scene scene = new Scene(root, 400, 400);
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
     }
-
+        
     /**
-     * @param args the command line arguments
+     * @param args the command line arguments */
      
     public static void main(String[] args) {
         launch(args);
-    } */
+    } 
     
 }
