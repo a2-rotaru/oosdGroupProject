d0a5ae3e4f294408cf087c83e8a7bda5e89cb4fd
Alexu <Alexu@DESKTOP-B5NRTRT>
2020-02-20 10:29:47 +0000

    MainView started plus photo

--- src/oosd2groupproject2/Manager.java
+++ src/oosd2groupproject2/Manager.java
@@ -3,34 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package oosd2groupproject2;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+//package oosd2groupproject2;
+//import javafx.beans.property.SimpleStringProperty;
+//import javafx.beans.property.StringProperty;
 /**
  *
  * @author Alexu
  */
-public class Manager {
-    private StringProperty name;
-    private String password;
+//public class Manager {
+   // private StringProperty name;
+   // private String password;
     
-    public Manager(String name, String password){
-        this.name = new SimpleStringProperty(name);
-        this.password = password;
+    //public Manager(String name, String password){
+      ///  this.name = new SimpleStringProperty(name);
+       // this.password = password;
         
-    }
+    //}
     
     //returning the name
-    public String getName(){
-        return name.get();
+   // public String getName(){
+      //  return name.get();
         
-    }
+    //}
     
-    public StringProperty nameProperty(){
-        return name;
-    }
+    //public StringProperty nameProperty(){
+      //  return name;
+    //}
     
-    public void setName (String name){
+   /* public void setName (String name){
         this.name.set(name);
     }
     
@@ -42,4 +42,4 @@ public class Manager {
         this.password = password;
         
     }
-}
+} */
